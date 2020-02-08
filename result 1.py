c98de773f9edac2bcfeb7a614f0340e0450adc9f
Kelompok 2 Prokom D <60640030+kelompok2prokomd@users.noreply.github.com>
2020-02-08 20:53:18 +0700

    Add files via upload

---
+++ result 1.py
@@ -0,0 +1,19 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================ RESTART: F:/tugas 1.py ============================
+Menghitung Volume Tabung
+Apabila tabung memiliki
+tinggi 10
+jari-jari lingkaran 7
+maka tabung memiliki volume 1540.0
+Menghitung Volume Kerucut
+Apabila kerucut memiliki
+tinggi 10
+jari-jari lingkaran 7
+maka kerucut memiliki volume 513.3333333333334
+Menghitung Volume Bola
+Apabila bola memiliki
+jari-jari lingkaran 7
+maka bola memiliki volume 1437.3333333333335
+>>> 
